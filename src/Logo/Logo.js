e6cf62c76e26fe9c60b6ade32f7c72ccd5f97d92
joecruz17 <joejcruz17@gmail.com>
2023-09-15 19:34:46 -0400

    app

---
+++ src/Logo/Logo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './TitleLogo.css'; // Import a CSS file for styling (you can create this file)
+
+function TitleLogo() {
+  return (
+    <div className="logo">
+      <img href="https://i.imgur.com/siKECBs.png" alt="Logo" />
+      <h1>Pokemon</h1>
+    </div>
+  );
+}
+
+export default TitleLogo;
